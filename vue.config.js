16fd86aaaaba9a26e407deba3bbe25fd90b05ac4
milesccoleman <milesccoleman@gmail.com>
2023-10-26 13:05:51 -0400

    again, different config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/psdb/' : '/',
+  publicPath: '/',
   devServer: {
-    allowedHosts: [".csb.app"], // <  Copy and paste the domain from the URL Codebox provides for your runtime here
+    allowedHosts: [".csb.app"], // Copy and paste the domain from the URL Codebox provides for your runtime here
   },
 };
